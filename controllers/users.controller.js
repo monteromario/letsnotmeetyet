67547f9c086d3c7528cfebae31d10ad2c55fe10e
monteromario <59968975+monteromario@users.noreply.github.com>
2021-02-23 00:09:58 +0100

    feat/creating first hbs views

---
+++ controllers/users.controller.js
@@ -0,0 +1,11 @@
+module.exports.profile = (req, res, next) => {
+      res.render("profile",);
+};
+
+module.exports.login = (req, res, next) => {
+      res.render("login",);
+};
+
+module.exports.register = (req, res, next) => {
+      res.render("register",);
+};
