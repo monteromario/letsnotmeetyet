4f900011d237f56babb2a71696a8ddbc2a960ee8
monteromario <59968975+monteromario@users.noreply.github.com>
2021-02-24 20:58:46 +0100

    feat/register

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/User.model");
+
 module.exports.profile = (req, res, next) => {
       res.render("profile",);
 };
@@ -8,4 +10,18 @@ module.exports.login = (req, res, next) => {
 
 module.exports.register = (req, res, next) => {
       res.render("register",);
+};
+
+module.exports.doRegister = (req, res, next) => {
+      //req.body.location = { type: 'Point', coordinates: [Math.random(), Math.random()] };
+      if (req.files.length > 0) {
+            let pictureArray = [];
+      
+      for (i=0; i<req.files.length; i++) {
+            pictureArray.push(req.files[i].path)
+            }
+      req.body.profilePictures = pictureArray;
+      }
+      User.create(req.body)
+      .then(() => res.send(req.body))
 };
