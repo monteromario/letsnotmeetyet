e43de0328c9f24414997ad7fef15b3e4e3c31de3
Laura <pedroscampoy@gmail.com>
2021-02-25 16:54:30 +0100

    initial map

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,27 +1,26 @@
 const User = require("../models/User.model");
 
 module.exports.profile = (req, res, next) => {
-      res.render("profile",);
+  res.render("profile");
 };
 
 module.exports.login = (req, res, next) => {
-      res.render("login",);
+  res.render("login");
 };
 
 module.exports.register = (req, res, next) => {
-      res.render("register",);
+  res.render("register");
 };
 
 module.exports.doRegister = (req, res, next) => {
-      //req.body.location = { type: 'Point', coordinates: [Math.random(), Math.random()] };
-      if (req.files.length > 0) {
-            let pictureArray = [];
-      
-      for (i=0; i<req.files.length; i++) {
-            pictureArray.push(req.files[i].path)
-            }
-      req.body.profilePictures = pictureArray;
-      }
-      User.create(req.body)
-      .then(() => res.send(req.body))
+  //req.body.location = { type: 'Point', coordinates: [Math.random(), Math.random()] };
+  if (req.files.length > 0) {
+    let pictureArray = [];
+
+    for (i = 0; i < req.files.length; i++) {
+      pictureArray.push(req.files[i].path);
+    }
+    req.body.profilePictures = pictureArray;
+  }
+  User.create(req.body).then(() => res.send(req.body));
 };
