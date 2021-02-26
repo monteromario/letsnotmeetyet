38f1a5c437f360acc6b43a2aac5c4b05ef0c796a
Laura <pedroscampoy@gmail.com>
2021-02-26 14:55:28 +0100

    click and update location in form

--- public/js/map.js
+++ public/js/map.js
@@ -4,32 +4,32 @@
 //       zoom: 14,
 //       center: marker || defaultPosition,
 //     });
-  
+
 //     let mapMarker = null
-  
+
 //     if (marker) {
 //       mapMarker = new google.maps.Marker({
 //         position: marker,
 //         map: map,
 //       });
 //     }
-  
+
 //     if (editable) {
 //       map.addListener('click', (e) => {
 //         if (mapMarker) {
 //           mapMarker.setMap(null)
 //         }
-    
+
 //         mapMarker = new google.maps.Marker({
 //           position: e.latLng,
 //           map: map
 //         })
-  
+
 //         const coordinates = e.latLng.toJSON()
-  
+
 //         document.getElementById('lat').value = coordinates.lat
 //         document.getElementById('lng').value = coordinates.lng
-  
+
 //         map.panTo(e.latLng)
 //       })
 //     }
@@ -38,8 +38,40 @@
 let map;
 
 function initMap() {
+  let mapMarker = null;
+  const defaultPosition = { lat: 40.4167278, lng: -3.7033387 };
+
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.4167278, lng: -3.7033387 },
-    zoom: 16,
+    center: defaultPosition,
+    zoom: 15,
+  });
+
+  //   let initMarker = new google.maps.Marker({
+  //     position: mapMarker || defaultPosition,
+  //     map: map,
+  //   });
+
+  mapMarker = new google.maps.Marker({
+    position: mapMarker || defaultPosition,
+    map: map,
+  });
+
+  map.addListener("click", (e) => {
+    if (mapMarker) {
+      mapMarker.setMap(null);
+    }
+
+    mapMarker = new google.maps.Marker({
+      position: e.latLng,
+      map: map,
+    });
+
+    const coordinates = e.latLng.toJSON();
+    console.log(coordinates);
+
+    document.getElementById("lat").value = coordinates.lat;
+    document.getElementById("lng").value = coordinates.lng;
+
+    map.panTo(e.latLng);
   });
 }
