240631dc3a75d63ded9983f3c04a03166cbb1189
Pedro <pedroscampoy@gmail.com>
2021-03-01 20:28:58 +0100

    circle profile pictures

--- public/js/mainMap.js
+++ public/js/mainMap.js
@@ -12,6 +12,8 @@ function initMap() {
   const markers = userLocations.map((user) => {
     let icon = {
       url: user.userPicture, // url
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(0, 0),
       scaledSize: new google.maps.Size(50, 50), // scaled size
     };
 
@@ -20,12 +22,32 @@ function initMap() {
       type: 'circle'
     };
 
-    return new google.maps.Marker({
+    const contentString = '<div id="content">' +
+      '<h5 id="firstHeading" class="firstHeading">' + user.username + '</h5>' +
+      '<div id="picture">' +
+      '<img src="' + user.userPicture + '" alt="' + user.username + 'width="100" height="100">' +
+      '</div>' +
+      '<br/>' +
+      '<form action = "/user/' + user.username + '">' +
+      '<input type="submit" value="PROFILE" />' +
+      "</form >" +
+      "</div>"
+
+    //"<input type='button' onClick=getDir() value='Go!'>" + TODO get route to marker
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+
+    const marker = new google.maps.Marker({
       position: { lat: user.coordinates[1], lng: user.coordinates[0] },
-      label: user.username,
+      //label: user.username,
       icon: icon,
       shape: shape,
       map: map
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
     });
   });
 
@@ -33,4 +55,5 @@ function initMap() {
     imagePath:
       "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
+
 }
