f0e8cabfa36caa3231e02d598e81585a101ccac3
monteromario <59968975+monteromario@users.noreply.github.com>
2021-03-10 00:00:18 +0100

    feat/profile-edit

--- config/routes.js
+++ config/routes.js
@@ -25,11 +25,11 @@ router.post(
   usersController.doRegister
 );
 
-router.get("/map", usersController.renderMap);
-
 router.get("/login", secure.isNotAuthenticated, usersController.login);
 router.post("/login", secure.isNotAuthenticated, usersController.doLogin);
 router.get("/login/help", secure.isNotAuthenticated, usersController.help);
+router.get("/reset/:email/:token", secure.isNotAuthenticated, usersController.reset);
+router.post("/reset", secure.isNotAuthenticated, usersController.doReset)
 router.post("/login/help", secure.isNotAuthenticated, usersController.doHelp);
 router.get("/activate/:token",secure.isNotAuthenticated, usersController.activate);
 router.get('/authenticate/google', passport.authenticate('google-auth', { scope: GOOGLE_SCOPES }))
@@ -39,8 +39,11 @@ router.get('/authenticate/facebook/callback', usersController.doLoginFacebook)
 router.get("/logout", secure.isAuthenticated, usersController.logout);
 router.get("/profile", secure.isAuthenticated, usersController.profile);
 router.get("/profile/edit", secure.isAuthenticated, usersController.editProfile);
-router.post("/profile/edit", secure.isAuthenticated, usersController.doEditProfile)
+router.post("/profile/edit", secure.isAuthenticated, upload.any(), usersController.doEditProfile)
 router.get("/profile/delete", secure.isAuthenticated, usersController.deleteProfile);
+router.get("/profile/resetPassword", secure.isAuthenticated, usersController.profileResetPassword);
+router.get("/map", usersController.renderMap);
+router.get("/favorites", secure.isAuthenticated, usersController.favorites);
 
 // router.get("/wishlist", secure.isAuthenticated, usersController.wishlist);
 // router.get("/users", secure.checkRole('ADMIN'), usersController.list);
